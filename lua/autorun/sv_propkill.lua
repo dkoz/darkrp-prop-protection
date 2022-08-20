afc22bfa4bd0b2b070f5f1e3ce379b6c8f0388c1
dkoz <drunkenkoz@gmail.com>
2022-08-20 10:37:29 -0400

    remove functions that did nothing, cleaned up the code

--- lua/autorun/sv_propkill.lua
+++ lua/autorun/sv_propkill.lua
@@ -1,75 +1,69 @@
-if (!SERVER) then return; end
+if (!SERVER) then return end
 
-local function notify(text)
-	for k,v in ipairs(player.GetAll()) do
-		v:PrintMessage(HUD_PRINTTALK, text);
-	end
-end
+hook.Add("PhysgunPickup", "PropProtectPickup", function(ply, ent)
+	if (ent:GetClass() != "prop_physics") then return end
 
-hook.Add("PhysgunPickup", "ZGPropKilLPickup", function(ply, ent)
-	if (ent:GetClass() != "prop_physics") then return; end
-	
-	ent.oldColor = ent:GetColor();
-	ent.oldMat = ent:GetMaterial();
-	
-	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 100));
-	ent:SetCollisionGroup(COLLISION_GROUP_WORLD);
+	ent.oldColor = ent:GetColor()
+	ent.oldMat = ent:GetMaterial()
+
+	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 100))
+	ent:SetCollisionGroup(COLLISION_GROUP_WORLD)
 end)
 
-hook.Add("PhysgunDrop", "ZGPropKilLDrop", function(ply, ent)	
+hook.Add("PhysgunDrop", "PropProtectThrow", function(ply, ent)
 end)
 
 local function freezeReal(ent)
 
 	if (timer.Exists(ent:EntIndex().."_waitForPlayersToLeaveToFreezeReal")) then
-		timer.Destroy(ent:EntIndex().."_waitForPlayersToLeaveToFreezeReal");
+		timer.Destroy(ent:EntIndex().."_waitForPlayersToLeaveToFreezeReal")
 	end
 	
-	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 255));
-	ent:SetMaterial(ent.oldMat);
-	ent:SetCollisionGroup(COLLISION_GROUP_NONE);
+	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 255))
+	ent:SetMaterial(ent.oldMat)
+	ent:SetCollisionGroup(COLLISION_GROUP_NONE)
 	
 end
 
 local function waitForPlayersToLeave(ent)
 	timer.Create(ent:EntIndex().."_waitForPlayersToLeaveToFreezeReal", 1, 0, function()
-		local min = ent:LocalToWorld(ent:OBBMins());
-		local max = ent:LocalToWorld(ent:OBBMaxs());
-		local entities = ents.FindInBox(min, max);
+		local min = ent:LocalToWorld(ent:OBBMins())
+		local max = ent:LocalToWorld(ent:OBBMaxs())
+		local entities = ents.FindInBox(min, max)
 		for k,v in ipairs(entities) do
 			if (v:IsPlayer()) then
-				return;
+				return
 			end
 		end
 		
-		freezeReal(ent);
+		freezeReal(ent)
 	end)
 end
 
-hook.Add("OnPhysgunFreeze", "ZGPropKillFreeze", function(weap, physobj, ent, ply)
-	if (ent:GetClass() != "prop_physics") then return; end
+hook.Add("OnPhysgunFreeze", "PropProtectFreeze", function(weap, physobj, ent, ply)
+	if (ent:GetClass() != "prop_physics") then return end
 	
-	local min = ent:LocalToWorld(ent:OBBMins());
-	local max = ent:LocalToWorld(ent:OBBMaxs());
-	local foundEnts = ents.FindInBox(min, max);
+	local min = ent:LocalToWorld(ent:OBBMins())
+	local max = ent:LocalToWorld(ent:OBBMaxs())
+	local foundEnts = ents.FindInBox(min, max)
 	
 	for k,v in ipairs(foundEnts) do
 		if (v:IsPlayer()) then
-			waitForPlayersToLeave(ent);
-			return;
+			waitForPlayersToLeave(ent)
+			return
 		end
 	end	
 	
-	freezeReal(ent);
+	freezeReal(ent)
 end)
 
-hook.Add("OnEntityCreated", "ZGPropKillSpawnProp", function(ent)
-	if (ent:GetClass() != "prop_physics") then return; end
+hook.Add("OnEntityCreated", "PropProtectSpawn", function(ent)
+	if (ent:GetClass() != "prop_physics") then return end
 	
-	ent.oldColor = ent.oldColor or ent:GetColor();
-	ent.oldMat = ent.oldMat or ent:GetMaterial();
+	ent.oldColor = ent.oldColor or ent:GetColor()
+	ent.oldMat = ent.oldMat or ent:GetMaterial()
 	
-	ent:SetRenderMode(RENDERMODE_TRANSALPHA);
-	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 100));
-	ent:SetCollisionGroup(COLLISION_GROUP_WORLD);
+	ent:SetRenderMode(RENDERMODE_TRANSALPHA)
+	ent:SetColor(Color(ent.oldColor.r, ent.oldColor.g, ent.oldColor.b, 100))
+	ent:SetCollisionGroup(COLLISION_GROUP_WORLD)
 end)
